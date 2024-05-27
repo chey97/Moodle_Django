4e54f249794071fb66a4953494b46d509735fac2
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-05-27 19:42:06 +0530

    Model updates

---
+++ frontend/src/components/Tables/TableClassroom.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TablePagination from "@mui/material/TablePagination";
+import { Typography, Box } from "@mui/material";
+
+import { ACCESS_TOKEN } from "../../constants";
+
+export default function BasicTable() {
+  const [students, setStudents] = useState([]);
+  const [headers, setHeaders] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        const response = await fetch("http://localhost:8000/classrooms/", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setStudents(data);
+
+        // Extract headers dynamically from the first student object
+        if (data.length > 0) {
+          const studentHeaders = Object.keys(data[0]);
+          setHeaders(studentHeaders);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: "50%" }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "10vh",
+          backgroundColor: "#1976d2",
+          border: "2px solid #ffff",
+          color: "white",
+          marginTop: "1vh",
+        }}
+      >
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="Dashboard"
+          sx={{
+            display: { xs: "none", md: "flex" },
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          Classrooms
+        </Typography>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {headers.map((header) => (
+                <TableCell key={header}>{header}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {students
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((student, index) => (
+                <TableRow key={index}>
+                  {headers.map((header) => (
+                    <TableCell key={header}>{student[header]}</TableCell>
+                  ))}
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={students.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
