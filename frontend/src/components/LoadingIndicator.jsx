3a1af80a992dcbdad43b5192a76a4aac6be61f5c
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-05-27 19:42:22 +0530

    Remove Components

--- frontend/src/components/LoadingIndicator.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/LoadingIndicator.css"
-
-const LoadingIndicator = () => {
-    return <div className="loading-container">
-        <div className="loader"></div>
-    </div>
-}
-
-export default LoadingIndicator
